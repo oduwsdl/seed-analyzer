815a05acef6068feb8d5f0170caf1d0de7e115c7
Sawood Alam <ibnesayeed@gmail.com>
2020-05-01 12:08:27 -0400

    Add logging and CLI arguments

--- seed-analyzer.sh
+++ seed-analyzer.sh
@@ -1,34 +1,61 @@
 #!/usr/bin/env bash
 
+# Exit immediately if anything goes wrong
+set -e
+
+# Set locale
 export LC_ALL=C
 
-opdir="derivatives"
+# Define a utility function to log on STDERR
+log() {
+    dt=$(date '+%Y/%d/%m %H:%M:%S.%N');
+    printf "[INFO $dt] %s\n" "$*" >&2
+}
+
+# CLI arguments with defaults
+coll=${1:-COVID19}
+seeddir=${2:-seeds}
+opdir=${3:-derivatives}
+
+# Configurable options
 src=(MC IIPC Merged)
 profile=(HxPx HxP1 H3P0 H1P0 DDom DSub DPth DQry DIni)
 weight=(1.00 0.50 0.10 0.05 0.01)
 
-rm $opdir/*
+log "Creating output directory (if missing): $opdir"
+mkdir -p $opdir
+log "Removing any $coll files from the output directory: $opdir/$coll*"
+rm -rf $opdir/$coll*
 
 for s in ${src[@]}
 do
+    seedf=$seeddir/$coll-$s.txt
     opfn=$opdir/$s
 
-    cp seeds/COVID19-$s.txt $opfn.SEED
+    log "Working on the seed file: $seedf"
+    log "Generating: $opfn.SEED"
+    cp $seedf $opfn.SEED
+    log "Generating: $opfn.URIR"
     sort -u $opfn.SEED > $opfn.URIR
+    log "Generating: $opfn.SURT"
     scripts/surter.py $opfn.URIR | sort -u > $opfn.SURT
 
     for p in ${profile[@]}
     do
+        log "Generating: $opfn.$p"
         scripts/uri_key_generator.py $p $opfn.URIR | sed 's|[,)/*]\+$||' | sort -u > $opfn.$p
     done
 
     for w in ${weight[@]}
     do
+        log "Generating: $opfn.W$w"
         scripts/mementomap.py $opfn.mm $w $opfn.SURT
         cut -d' ' -f 1 $opfn.mm | sed 's|[,)/*]\+$||' | sort -u > $opfn.W$w
         rm $opfn.mm
     done
 done
+
+log "Analyzing generated files to report summary"
 
 echo -n "Variant"
 for s in ${src[@]}
@@ -51,3 +78,5 @@ do
     done
     echo
 done
+
+log "All done!"
